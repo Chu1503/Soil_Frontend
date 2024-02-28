2ed785d45e1286d4c04634798b21e241f84374b5
Shiv Akash <shivakash2003@gmail.com>
2024-02-29 02:32:48 +0530

    ui change

--- src/App.jsx
+++ src/App.jsx
@@ -110,7 +110,7 @@ const App = () => {
         />
       </div>
       <button
-        className="bg-blue-500 hover:bg-blue-700 text-yellow-50 font-bold py-2 px-4 rounded"
+        className="bg-blue-500 hover:bg-blue-700 text-yellow-50 font-bold mt-5 py-2 px-4 rounded"
         onClick={handleButtonClick}
       >
         Calculate
